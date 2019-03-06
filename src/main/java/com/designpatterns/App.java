d15a9b5469af9ae471208fc2d3ca51f87c4cb816
Alfons Lange <alfons_lange97@hotmail.com>
2019-03-06 09:04:10 +0100

    Add TODO list comment

--- src/main/java/com/designpatterns/App.java
+++ src/main/java/com/designpatterns/App.java
@@ -4,9 +4,21 @@ import com.designpatterns.controller.Controller;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-/**
- * Hello world!
+/** TODO
+ * Intuitivt gränssnitt
+ * Rita olika typer av former (linje, oval, flerhörning, ...)
+ * Markera objekt i vyn för att ändra egenskaper
+ * Minimum egenskaper: färg, linjebredd, fyllning
+ * Spara / läsa in rit-filer (objektserialisering är OK)
  *
+ * [.] MVC (Olika paket för olika delar)
+ * [.] Subject-Observer, uppdatera vyer när modell ändras
+ * [ ] Command, för undo/redo funktion
+ * [ ] Composite, för att gruppera objekt, hantera gemensamma egenskaper
+ * [ ] Template-metod där det kan behövas
+ * [.] Facade, dölj komplexa modeller
+ * [.] Skapelsemönster Ex. Prototype, för att skapa de ritbara objekten
+ * [ ] Automatisk uppdatering av knappar/menyalternativ från de ritbara typer som finns i modellen
  */
 public class App extends Application {
     public static void main( String[] args ) {
